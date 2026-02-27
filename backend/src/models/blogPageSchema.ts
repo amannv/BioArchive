eead85d44ed0b236cac0fdfbe73b49416aaa184f
Aman <aman.ver9345@gmail.com>
2026-02-27 21:50:00 +0530

    tested all routes and did some changes in blogpage schema

--- backend/src/models/blogPageSchema.ts
+++ backend/src/models/blogPageSchema.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const blogPageSchema = new mongoose.Schema({
-    avatar: { type: String, default: "" },
-    username: { type: String, default: "", unique: true },
-    github: { type: String, default: "" },
-    instagram: { type: String, default: "" },
-    linkedin: { type: String, default: "" },
-    twitter: { type: String, default: "" },
-    userId: { type: mongoose.Schema.ObjectId, ref: "User" },
-})
+  avatar: { type: String, default: "" },
+  username: { type: String, default: "", unique: true },
+  github: { type: String, default: "" },
+  instagram: { type: String, default: "" },
+  linkedin: { type: String, default: "" },
+  twitter: { type: String, default: "" },
+  userId: { type: mongoose.Schema.ObjectId, ref: "User" },
+});
 
 export const blogPageModel = mongoose.model("BlogPages", blogPageSchema);
